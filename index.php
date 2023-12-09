8852dac2eacb871a166fc6dcc72ab86a6288aeb1
«GitHub» <sprokit1@mail.ru>
2023-12-09 22:50:56 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Лабораторные работы</title>
+<style>
+.title{
+text-align: center;
+border-bottom: solid;
+padding-bottom: 20px;
+}
+.menu{
+margin-top: 50px;
+}
+.c {
+border: 1px solid #333; /* Рамка */
+display: inline-block;
+padding: 10px 20px; /* Поля */
+margin: 10px 20px;
+text-decoration: none;  
+color: #000; /* Цвет текста */
+}
+.c:hover {
+box-shadow: 0 0 5px rgba(0,0,0,0.3); /* Tень background: linear-gradient (to bottom, #fcfff4,
+#e9e9ce); /* Градиент */
+color: #a00;
+}
+</style>
+</head>
+<body>
+<div class="title">
+<h1>
+ЛАБОРАТОРНЫЕ РАБОТЫ
+</h1>
+<h3>
+Выполнил: Михеев Александр
+</h3>
+</div>
+<div class="menu">
+<a href="1lab.php" class="c">Лабораторная работа №1</a>
+<a href="2lab.php" class="c">Лабораторная работа №2</a> 
+<a href="3lab.php" class="c">Лабораторная работа №3</a>
+<a href="4lab.php" class="c">Лабораторная работа №4</a> 
+<a href="5lab.php" class="c">Лабораторная работа №5</a>
+<a href="6lab.php" class="c">Лабораторная работа №6</a> 
+<a href="labwork7-8/1ab7-8.php" class="c">Лабораторная работа №7-8</a>
+</div>
+</body>
+</html>
